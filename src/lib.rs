11ad6a874d56516c30d4c098cff2878c754bbfef
messense <messense@icloud.com>
2015-05-27 00:33:17 +0800

    Implement Drop for OpenCC

--- src/lib.rs
+++ src/lib.rs
@@ -14,33 +14,48 @@ extern {
 
 pub struct OpenCC {
     pub config: String,
+    libopencc: *mut c_void,
 }
 
 impl OpenCC {
     pub fn new(config: &str) -> OpenCC {
-        OpenCC {
-            config: config.to_string(),
+        let _config = config.to_string();
+        let config_ptr = _config.as_ptr();
+        unsafe {
+            OpenCC {
+                config: _config,
+                libopencc: opencc_open(config_ptr as *const c_char),
+            }
         }
     }
 
     pub fn convert(&self, text: &str) -> String {
         unsafe {
-            let config_ptr = self.config.as_ptr();
-            let opencc = opencc_open(config_ptr as *const c_char);
             let text_ptr = text.as_ptr();
-            let c_ptr = opencc_convert_utf8(opencc, text_ptr as *const c_char, text.len() as size_t);
+            let c_ptr = opencc_convert_utf8(self.libopencc, text_ptr as *const c_char, text.len() as size_t);
             let c_str = CStr::from_ptr(c_ptr);
             let str_buf: String = str::from_utf8(c_str.to_bytes()).unwrap().to_owned();
             opencc_convert_utf8_free(c_ptr);
-            opencc_close(opencc);
             str_buf
         }
+    }
+
+    pub fn close(&self) {
+        unsafe {
+            opencc_close(self.libopencc);
+        }
+    }
+}
+
+impl Drop for OpenCC {
+    fn drop(&mut self) {
+        self.close();
     }
 }
 
 #[test]
 fn test_convert() {
     let opencc = OpenCC::new("t2s.json");
-    let result = opencc.convert("乾坤一擲");
-    assert_eq!("乾坤一掷".to_string(), result);
+    assert_eq!("乾坤一掷".to_string(), opencc.convert("乾坤一擲"));
+    assert_eq!("开放中文转换".to_string(), opencc.convert("開放中文轉換"));
 }
--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ use opencc::OpenCC;
 fn main() {
     let cc = OpenCC::new("t2s.json");
     println!("{}", cc.convert("乾坤一擲"));
+    println!("{}", cc.convert("開放中文轉換"));
 }
