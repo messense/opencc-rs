0c182b8c5fd5447793d031f78b5fbd98aae34683
messense <messense@icloud.com>
2018-01-31 09:45:47 +0800

    Simplify interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -23,14 +23,14 @@
 //!
 //! fn main() {
 //!     let cc = OpenCC::new("t2s.json");
-//!     println!("{}", cc.convert("乾坤一擲").unwrap());
-//!     println!("{}", cc.convert("開放中文轉換").unwrap());
+//!     println!("{}", cc.convert("乾坤一擲"));
+//!     println!("{}", cc.convert("開放中文轉換"));
 //! }
 //! ```
 extern crate libc;
 use libc::{size_t, c_void, c_char};
 
-use std::cell::Cell;
+use std::ptr;
 use std::ffi::{CStr, CString};
 
 #[link(name = "opencc")]
@@ -45,7 +45,6 @@ extern {
 pub struct OpenCC {
     /// Configuration file
     pub config: String,
-    _is_closed: Cell<bool>,
     libopencc: *mut c_void,
 }
 
@@ -61,7 +60,6 @@ impl OpenCC {
         unsafe {
             OpenCC {
                 config: config.to_string(),
-                _is_closed: Cell::new(false),
                 libopencc: opencc_open(c_config.as_ptr()),
             }
         }
@@ -72,42 +70,29 @@ impl OpenCC {
     /// # Examples
     /// ```
     /// let cc = opencc::OpenCC::new("t2s.json");
-    /// cc.convert("乾坤一擲").unwrap();
-    /// cc.convert("開放中文轉換").unwrap();
+    /// cc.convert("乾坤一擲");
+    /// cc.convert("開放中文轉換");
     /// ```
-    pub fn convert(&self, text: &str) -> Option<String> {
-        if self._is_closed.get() {
-            None
-        } else {
-            unsafe {
-                let c_text = CString::new(text).unwrap();
-                let c_ptr = opencc_convert_utf8(self.libopencc, c_text.as_ptr(), text.len() as size_t);
-                if c_ptr.is_null() {
-                    None
-                } else {
-                    let c_str = CStr::from_ptr(c_ptr);
-                    let str_buf = c_str.to_str().unwrap().to_owned();
-                    opencc_convert_utf8_free(c_ptr);
-                    Some(str_buf)
-                }
-            }
+    pub fn convert(&self, text: &str) -> String {
+        unsafe {
+            let c_text = CString::new(text).unwrap();
+            let c_ptr = opencc_convert_utf8(self.libopencc, c_text.as_ptr(), text.len() as size_t);
+            let c_str = CStr::from_ptr(c_ptr);
+            let str_buf = c_str.to_str().unwrap().to_owned();
+            opencc_convert_utf8_free(c_ptr);
+            str_buf
         }
     }
 
     /// Close the underlying libopencc.
     /// Will be called automatically when the variable gets out of scope.
-    pub fn close(&self) {
-        if !self._is_closed.get() {
+    fn close(&mut self) {
+        if !self.libopencc.is_null() {
             unsafe {
                 opencc_close(self.libopencc);
             }
-            self._is_closed.set(true);
+            self.libopencc = ptr::null_mut();
         }
-    }
-
-    /// Is the underlying libopencc closed
-    pub fn is_closed(&self) -> bool {
-        self._is_closed.get()
     }
 
     /// Returns the last error message
@@ -143,18 +128,8 @@ mod tests {
     #[test]
     fn test_simple_convert() {
         let cc = OpenCC::new("t2s.json");
-        assert_eq!("乾坤一掷", &cc.convert("乾坤一擲").unwrap());
-        assert_eq!("开放中文转换", &cc.convert("開放中文轉換").unwrap());
-    }
-
-    #[test]
-    fn test_opencc_close() {
-        let cc = OpenCC::new("t2s.json");
-        assert_eq!("乾坤一掷", &cc.convert("乾坤一擲").unwrap());
-        assert_eq!(false, cc.is_closed());
-        cc.close();
-        assert_eq!(true, cc.convert("開放中文轉換").is_none());
-        assert_eq!(true, cc.is_closed());
+        assert_eq!("乾坤一掷", &cc.convert("乾坤一擲"));
+        assert_eq!("开放中文转换", &cc.convert("開放中文轉換"));
     }
 
     #[test]
