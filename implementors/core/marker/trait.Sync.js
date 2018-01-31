177e2297b8aedaf64942c7b097730797e8beef17
Travis CI User <travis@example.org>
2018-01-31 01:58:51 +0000

    Documentation for messense/opencc-rs@65c64b3

---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["opencc"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"opencc/struct.OpenCC.html\" title=\"struct opencc::OpenCC\">OpenCC</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
